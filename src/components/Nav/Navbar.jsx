5663da65fd782c578827687d326813022e63ff0a
Gaurav <corecomputer143@gmail.com>
2024-01-24 00:31:18 +0530

    Add Mobile Nav

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../assets/img/logo.png";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const [state, setState] = useState(false);
   const location = useLocation();
   return (
     <header>
@@ -11,6 +12,71 @@ const Navbar = () => {
         <h4>
           Bashar Institute <span>of Computer</span>
         </h4>
+      </div>
+      <div className="m-right">
+        <span onClick={() => setState(true)}>
+          <i className="bx bx-menu"></i>
+        </span>
+        {state && (
+          <div className="mobile-nav">
+            <div className="mobile-nav-top">
+              <span onClick={() => setState(false)}>
+                <i className="bx bx-left-arrow-alt"></i>
+              </span>
+            </div>
+
+            <div className="mobile-nav-content">
+              <Link
+                className={location?.pathname === "/" ? "link active" : "link"}
+                to="/"
+              >
+                Home
+              </Link>
+              <Link
+                className={
+                  location?.pathname === "/about" ? "link active" : "link"
+                }
+                to="/about"
+              >
+                About
+              </Link>
+              <Link
+                className={
+                  location?.pathname === "/course" ? "link active" : "link"
+                }
+                to="/course"
+              >
+                Courses
+              </Link>
+              <Link
+                className={
+                  location?.pathname === "/certificate/search"
+                    ? "link active"
+                    : "link"
+                }
+                to="/certificate/search"
+              >
+                Certificate
+              </Link>
+              <Link
+                className={
+                  location?.pathname === "/addmission" ? "link active" : "link"
+                }
+                to="/addmission"
+              >
+                Addmission
+              </Link>
+              <Link
+                className={
+                  location?.pathname === "/gallery" ? "link active" : "link"
+                }
+                to="/gallery"
+              >
+                Gallery
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
       <div className="right">
         <Link
